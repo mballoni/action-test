9266a08c3ba5f9b13190c1eb2d274a25972162be
Andrew Stevko <andy.stevko@gmail.com>
2020-05-14 15:29:26 -0700

    output stuff to stdout first

--- calc-version.sh
+++ calc-version.sh
@@ -3,8 +3,8 @@ export VERSION=19.2.5
 export FULL_TAG=19.2.5-beta
 export SHORT_TAG=beta
 
+echo "VERSION=$VERSION; FULL_TAG=$FULL_TAG; SHORT_TAG=$SHORT_TAG"
+
 echo "::set-env name=VERSION::$VERSION"
 echo "::set-env name=FULL_TAG::$FULL_TAG"
 echo "::set-env name=SHORT_TAG::$SHORT_TAG"
-
-echo "VERSION=$VERSION; FULL_TAG=$FULL_TAG; SHORT_TAG=$SHORT_TAG"
